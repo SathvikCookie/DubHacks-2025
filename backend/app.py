307fbd88cf205bcb6a50d8131603cef24ebe083f
Shriyan Dey <72957329+ShriyanDey@users.noreply.github.com>
2025-10-18 17:20:09 -0700

    ICV v1.

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
+# from flask_cors import CORS
 from config import Config
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    CORS(app)
+    # CORS(app)
     
     # Register blueprints
     from api.stories import stories_bp
